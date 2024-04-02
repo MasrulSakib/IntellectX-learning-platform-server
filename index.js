1dc1a9ff65c78b792ffe2d1e4bc0706792b752ab
MasrulSakib <Masrul.Sakib69@gmail.com>
2024-04-03 04:31:01 +0600

    category list json added

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const courses = require('./Data/course.json');
+const categoryList = require('./Data/categoryList.json');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('This is home server and it is running.....')
+})
+
+app.get('/categories-list', (req, res) => {
+    res.send(categoryList);
 })
 
 app.get('/category/:category_id', (req, res) => {
@@ -25,6 +30,9 @@ app.get('/courses/:_id', (req, res) => {
     const id = req.params._id;
     const courseData = courses.find(course => course._id === id)
     res.send(courseData);
+})
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
